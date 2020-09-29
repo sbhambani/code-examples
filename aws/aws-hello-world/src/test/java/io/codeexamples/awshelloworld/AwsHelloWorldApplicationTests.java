141bc5b67ee48048f822a79cf376b226843a78b6
Sunil Bhambani -X (sunbhamb - PERSISTENT SYSTEMS INC at Cisco) <sunbhamb@cisco.com>
2020-09-30 01:44:10 +0530

    Initial commit

---
+++ aws/aws-hello-world/src/test/java/io/codeexamples/awshelloworld/AwsHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codeexamples.awshelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
